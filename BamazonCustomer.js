1be3911715131e6096889b3c3327c2a87e20bd8a
Brad <bradleynielsen@gmail.com>
2016-10-29 16:53:05 -0500

    adding order deprication

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -11,39 +11,73 @@ var connection = mysql.createConnection({
     database: "bamazon"
 })
 
+
+////FUNCTIONS
+var orderItem = function(ItemID, ammount){
+
+	connection.query('UPDATE products SET StockQuantity=' + ammount + ' WHERE ItemID=' + ItemID);
+	
+}
+
+
 // first display ids, names, and prices of products for sale.
 var displayAllProducts = function() {	
 	connection.query('SELECT ItemID, ProductName, Price FROM bamazon.products', function(error, result) {
-	    console.log("-----------------------------------");
-	    console.log("ItemID" + " | " + "Product Name" + " | " + "Price");
+	    	console.log("----------------------------------------------------------------");
 	    for (var i = 0; i < result.length; i++) {
-	        console.log(result[i].ItemID + " | " + result[i].ProductName + " | " + result[i].Price);
+	        console.log("ItemID: " + result[i].ItemID + " | " + "Product Name: " + result[i].ProductName + " | " + "Price: $" + result[i].Price);
+	    	console.log("----------------------------------------------------------------");
+	    	
 	    }
-	    console.log("-----------------------------------");
-	})
+	whatToBuyID();
+	});
 }
 
-var whatToBuy = function(){}
+// //check to see if there are enogh in stock
+// var numberInStock = function(answers){
+// }
 
+// The app should then prompt users with two messages.
+var whatToBuyID = function(){
+		inquirer.prompt([{
+// The first should ask them the ID of the product they would like to buy.
+			type: "input",
+		    name: "ItemID",
+			message: "What is the ID of the product they would like to buy?"
+		},{
+// The second message should ask how many units of the product they would like to buy.
+			type: "input",
+		    name: "orderAmmount",
+			message: "How many would you like to buy?"
+		}]).then(function(answers) { 
+			// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+			connection.query('SELECT StockQuantity FROM bamazon.products WHERE ItemID ='+ answers.ItemID, function(error, result) {
+				
+				if (answers.orderAmmount <= result[0].StockQuantity) {
+					// However, if your store does have enough of the product, you should fulfill the customer's order.
+					console.log("Make the order!");
+					var orderitemID = answers.ItemID;
+					var orderAmmount = answers.orderAmmount
+					orderItem(orderitemID, orderAmmount);
 
+				} else {
+				// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+				console.log("There are only " + result[0].StockQuantity + " items left. Please order " + result[0].StockQuantity + " items or less.");
+				}
+
+			})
+			displayAllProducts();
+		})
+}
 
 
 connection.connect(function(error) {
     if (error) throw error;
-    	displayAllProducts();
+    	displayAllProducts();	
+
 })
 
 
-
-// The first should ask them the ID of the product they would like to buy.
-
-// The second message should ask how many units of the product they would like to buy.
-
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-// However, if your store does have enough of the product, you should fulfill the customer's order.
 
 // This means updating the SQL database to reflect the remaining quantity.
 // Once the update goes through, show the customer the total cost of their purchase.
